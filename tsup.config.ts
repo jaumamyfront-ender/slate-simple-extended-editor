6022cdec32949d65370798a2b07152355f12a246
yaumamyfront-ender <nepobedimyjpobedit@gmail.com>
2025-01-25 00:59:30 +0100

    add-build-trought-tsup-beta

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["./src/components/extendedTextarea/extendedTextEditor.tsx"], // Входной файл
+  format: ["esm", "cjs"], // Форматы сборки
+  dts: true, // Генерация типов (если используешь TypeScript)
+  splitting: false, // Можно включить, если нужно деление на чанки
+  loader: {
+    ".svg": "file", // Обрабатывает SVG как файлы
+  },
+  clean: true, // Удаляет старую сборку перед новой
+});
